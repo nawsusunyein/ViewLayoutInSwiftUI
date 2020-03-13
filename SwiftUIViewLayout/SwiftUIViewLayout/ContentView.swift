1410d4d61214d8c84f905add3a02c5ff60a10611
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2020-03-13 17:20:30 +0630

    create project

---
+++ SwiftUIViewLayout/SwiftUIViewLayout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIViewLayout
+//
+//  Created by techfun on 2020/03/13.
+//  Copyright © 2020 Naw Su Su Nyein. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
